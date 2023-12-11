cff7723eecd33de1e5a8bb079be72205655686b7
ShyamL1319 <sshyamlalchahuan@gmail.com>
2023-12-11 21:55:23 +0530

    Comment and string method added
    Signed-off-by: ShyamL1319 <sshyamlalchahuan@gmail.com>

--- LongestPalendromicSubstring.java
+++ LongestPalendromicSubstring.java
@@ -49,7 +49,7 @@
 
 public class LongestPalendromicSubstring {
     public static void main(String[] args) {
-        char s[] = {'a', 'b', 'a', 'c', 'a', 'b'};
+        char[] s = {'a', 'b', 'a', 'c', 'a', 'b'};
         int ans = 1;
 
         for (int i = 0; i < s.length; i++) {
@@ -63,7 +63,7 @@ public class LongestPalendromicSubstring {
     }
 
 
-    public static int getPalendromicLength(char [] s, int i, int j ){
+    public static int getPalendromicLength(char [] s, int i, int j ) {
         int len = 0;
         int N = s.length;
         if (i == j) {
