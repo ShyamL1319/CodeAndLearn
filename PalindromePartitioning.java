4b2d11e780fca0aeb1302721231470b1001c9ec4
Shyam Lal <sshyamlalchauhan@gmail.com>
2024-01-15 19:42:25 +0530

    Palindrome Partitioning II --- Optimised
    Signed-off-by: Shyam Lal <sshyamlalchauhan@gmail.com>

--- PalindromePartitioning.java
+++ PalindromePartitioning.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Date: 15/01/2024:19:35
  * User: shyamlal
@@ -50,48 +52,94 @@
  * Return 1 since the palindrome partitioning ["aa","b"] could be produced using 1 cut.
  */
 public class PalindromePartitioning {
+    //    private static void makePalendrome(String A, int[][] dp) {
+//        for (int d = 0; d < A.length(); d++) {
+//            for (int i = 0, j = d; j < A.length(); i++, j++) {
+//                if (i == j) {
+//                    dp[i][j] = 1;
+//                } else {
+//                    if (A.charAt(i) == A.charAt(j)) {
+//                        int left = i + 1;
+//                        int right = j - 1;
+//                        if (right < left) {
+//                            dp[i][j] = 1;
+//                        } else {
+//                            dp[i][j] = dp[left][right];
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+//
+//    public static int minCut(String A) {
+//        int[] dp = new int[A.length()];
+//        int[][] dpStore = new int[A.length()][A.length()];
+//        makePalendrome(A, dpStore);
+//        for (int i = 0; i < A.length(); i++) {
+//            dp[i] = i;
+//            if (dpStore[0][i] == 1) {
+//                dp[i] = 0;
+//            } else {
+//                for (int j = i; j >= 1; j--) {
+//                    if (dpStore[j][i] == 1) {
+//                        dp[i] = Math.min(dp[i], (dp[j - 1] + 1));
+//                    }
+//                }
+//            }
+//        }
+//
+//        return dp[A.length() - 1];
+//
+//    }
+    int[] dp;
+
     public static void main(String[] args) {
-        System.out.println(minCut("aba"));
+        PalindromePartitioning pp = new PalindromePartitioning();
+        System.out.println(pp.minCut("aba"));
     }
 
-    private static void makePalendrome(String A, int[][] dp) {
-        for (int d = 0; d < A.length(); d++) {
-            for (int i = 0, j = d; j < A.length(); i++, j++) {
-                if (i == j) {
-                    dp[i][j] = 1;
-                } else {
-                    if (A.charAt(i) == A.charAt(j)) {
-                        int left = i + 1;
-                        int right = j - 1;
-                        if (right < left) {
-                            dp[i][j] = 1;
-                        } else {
-                            dp[i][j] = dp[left][right];
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    public static int minCut(String A) {
-        int[] dp = new int[A.length()];
-        int[][] dpStore = new int[A.length()][A.length()];
-        makePalendrome(A, dpStore);
-        for (int i = 0; i < A.length(); i++) {
-            dp[i] = i;
-            if (dpStore[0][i] == 1) {
-                dp[i] = 0;
+    public boolean isPelindrome(String str, int i, int j) {
+        int n = str.length();
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
             } else {
-                for (int j = i; j >= 1; j--) {
-                    if (dpStore[j][i] == 1) {
-                        dp[i] = Math.min(dp[i], (dp[j - 1] + 1));
-                    }
-                }
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+
+    public int findCuts(String str, int n, int i) {
+        if (i == n) {
+            return 0;
+        }
+
+        if (dp[i] != -1) {
+            return dp[i];
+        }
+
+        int minCost = Integer.MAX_VALUE;
+        for (int j = i; j < n; j++) {
+            if (isPelindrome(str, i, j)) {
+                int cost = 1 + findCuts(str, n, j + 1);
+                minCost = Math.min(minCost, cost);
             }
         }
 
-        return dp[A.length() - 1];
+        dp[i] = minCost;
+        return minCost;
+    }
 
+    public int minCut(String A) {
+        int n = A.length();
+        if (n == 1 || isPelindrome(A, 0, n - 1)) {
+            return 0;
+        }
+        dp = new int[n];
+        Arrays.fill(dp, -1);
+        return findCuts(A, n, 0) - 1;
     }
 }
