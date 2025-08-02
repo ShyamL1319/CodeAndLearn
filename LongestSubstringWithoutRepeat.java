2910c60cbec2d9a07edb09c233c888ea7d1056b1
Shyam Lal <sshyamlalchauhan@gmail.com>
2025-08-02 13:16:23 +0530

    day 6 - count reverse pairs

--- LongestSubstringWithoutRepeat.java
+++ LongestSubstringWithoutRepeat.java
@@ -871,5 +871,60 @@ public class LongestSubstringWithoutRepeat {
         }
     }
 
+    public int reversPair(int nums[]) {
+        int res = mergeSortR(nums, 0, nums.length - 1);
+        return res;
+    }
+
+    public int mergeSortR(int nums[], int low, int high) {
+        if (low >= high)
+            return 0;
+        int mid = (low + high) / 2;
+        int cnt = mergeSortR(nums, low, mid);
+        cnt += mergeSortR(nums, mid + 1, high);
+        cnt += mergeR(nums, low, mid, high);
+        return cnt;
+
+    }
+
+    public int mergeR(int nums[], int low, int mid, int high) {
+        int cnt = 0;
+        int size = high - low + 1;
+        int temp[] = new int[size];
+        int j = mid + 1;
+        for (int i = low; i <= mid; i++) {
+
+            while (j <= high && nums[i] > 2 * nums[j]) {
+                j++;
+            }
+            cnt += j - ((mid + 1));
+        }
+
+        int left = low;
+        int right = mid + 1;
+        int idx = 0;
+        while (left <= mid && right <= high) {
+            if (nums[left] <= nums[right]) {
+                temp[idx++] = nums[left++];
+            } else {
+                temp[idx++] = nums[right++];
+            }
+        }
+
+        while (left <= mid) {
+            temp[idx++] = nums[left++];
+        }
+
+        while (right <= high) {
+            temp[idx++] = nums[right++];
+        }
+
+        idx = 0;
+        while (idx < size) {
+            nums[low + idx] = temp[idx];
+            idx++;
+        }
+        return cnt;
+    }
 
 }
